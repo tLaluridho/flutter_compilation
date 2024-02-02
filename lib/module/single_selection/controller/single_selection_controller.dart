c680667d888b6f1b4c5cbd21cf3dcd0e7640858b
LaluRidho <ridhotaufik2003@gmail.com>
2024-02-02 22:17:13 +0800

    membuat fitur single selection

--- lib/module/single_selection/controller/single_selection_controller.dart
+++ lib/module/single_selection/controller/single_selection_controller.dart
@@ -1,23 +1,33 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_compilation/core.dart';
 import '../view/single_selection_view.dart';
 
 class SingleSelectionController extends State<SingleSelectionView> {
-    static late SingleSelectionController instance;
-    late SingleSelectionView view;
+  static late SingleSelectionController instance;
+  late SingleSelectionView view;
 
-    @override
-    void initState() {
-        instance = this;
-        super.initState();
-    }
+  @override
+  void initState() {
+    instance = this;
+    super.initState();
+  }
 
-    @override
-    void dispose() => super.dispose();
+  @override
+  void dispose() => super.dispose();
 
-    @override
-    Widget build(BuildContext context) => widget.build(context, this);
+  @override
+  Widget build(BuildContext context) => widget.build(context, this);
+  List category = [
+    "Sembako",
+    "snack",
+    "minuman",
+    "Baju",
+    "Celana",
+  ];
+
+  int selectedIndex = -1;
+  updateIndex(int newIndex) {
+    selectedIndex = newIndex;
+    setState(() {});
+  }
 }
-        
-    
--- lib/module/single_selection/view/single_selection_view.dart
+++ lib/module/single_selection/view/single_selection_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_compilation/core.dart';
 import '../controller/single_selection_controller.dart';
@@ -7,18 +6,45 @@ class SingleSelectionView extends StatefulWidget {
   const SingleSelectionView({Key? key}) : super(key: key);
 
   Widget build(context, SingleSelectionController controller) {
-  controller.view = this;
+    controller.view = this;
     return Scaffold(
       appBar: AppBar(
         title: const Text("SingleSelection"),
         actions: const [],
-        ),
-        body: SingleChildScrollView(
-        child: Container(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-            children: const [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Text(
+              "${controller.selectedIndex}",
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.category.length,
+                padding: EdgeInsets.zero,
+                clipBehavior: Clip.none,
+                itemBuilder: (context, index) {
+                  var item = controller.category[index];
+                  bool selected = controller.selectedIndex == index;
+                  return Card(
+                    color: selected ? Colors.black : Colors.white,
+                    child: ListTile(
+                      onTap: () => controller.updateIndex(index),
+                      title: Text(
+                        "$item",
+                        style: TextStyle(color: selected ? Colors.white : null),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
@@ -27,4 +53,3 @@ class SingleSelectionView extends StatefulWidget {
   @override
   State<SingleSelectionView> createState() => SingleSelectionController();
 }
-    
